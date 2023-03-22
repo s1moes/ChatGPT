c07fdefb5516c45f89cc749816955d20f2c34b3e
Francisco <franciscosimoes2002@hotmail.com>
2023-03-22 16:15:55 +0000

    Generate Chat and Message bodys

---
+++ src/main/java/pt/franciscosimoes/chatgpt/model/Chat.java
@@ -0,0 +1,8 @@
+package pt.franciscosimoes.chatgpt.model;
+
+import java.util.List;
+
+public class Chat {
+	private String prompt;
+	private List<Message> message;
+}
---
+++ src/main/java/pt/franciscosimoes/chatgpt/model/Message.java
@@ -0,0 +1,29 @@
+package pt.franciscosimoes.chatgpt.model;
+
+public class Message {
+
+	private String subject;
+	private String content;
+	
+	public Message () {
+		
+	}
+	public Message(String subject, String content) {
+		this.subject.
+		this.content.
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
