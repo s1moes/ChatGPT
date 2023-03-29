e73d1de381737c9c6a983e391422231249c73fd1
Francisco <franciscosimoes2002@hotmail.com>
2023-03-29 16:14:20 +0100

    greeting

--- src/main/java/pt/franciscosimoes/chatgpt/util/FileUtils.java
+++ src/main/java/pt/franciscosimoes/chatgpt/util/FileUtils.java
@@ -9,18 +9,16 @@ import java.util.List;
 public class FileUtils {
 	
 	public static List<String> readLines(String filePath)throws IOException { 
+	
 	List<String> result = new ArrayList<>();
 	
 	BufferedReader reader = new BufferedReader(new FileReader(filePath));
 	
 	String line;
-	
 	try {
-	while ((line = reader.readLine()) != null) {
-		result.add(line);
-	}
-	}catch(IOException e) {
-		throw e;
+		while ((line = reader.readLine()) != null) {
+			result.add(line);
+		}
 	}finally {
 		reader.close();
 	}
