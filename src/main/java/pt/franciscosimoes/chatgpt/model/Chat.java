4f9824963862e9d17b9cc1b3aeca76ef1cee95ac
Francisco <franciscosimoes2002@hotmail.com>
2023-04-19 14:13:05 +0100

    Improve chat class

--- src/main/java/pt/franciscosimoes/chatgpt/model/Chat.java
+++ src/main/java/pt/franciscosimoes/chatgpt/model/Chat.java
@@ -4,35 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Chat {
-	private String prompt;
-	private List<Message> messages;
-	
-//	public Chat() {
-//		this.message = new ArrayList<>();
-//	}
-	
+	private final String prompt;
+	private final List<Message> messages;
+
+	public Chat(String prompt) {
+		super();
+		this.prompt = prompt;
+		this.messages = new ArrayList<>();
+	}
+
 	public String getPrompt() {
 		return prompt;
 	}
+
 	public List<Message> getMessages() {
 		return messages;
 	}
-	
-	public void setPrompt(String prompt) {
-		this.prompt = prompt;
-	}
-	
-	public void setMessages(List<Message> messages) {
-		if (messages == null) {
-			throw new IllegalArgumentException("messages list cannot be null");
-		}
-		this.messages = messages;
-	}
-	
+
 	public void addMessage(Message message) {
-		if (this.messages == null) {
-			this.messages = new ArrayList<>();
-		}
 		this.messages.add(message);
 	}
 }
