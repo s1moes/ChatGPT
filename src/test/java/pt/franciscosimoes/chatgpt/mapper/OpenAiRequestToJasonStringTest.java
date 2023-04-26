fc516556d6a8e624069f4c7bdd4cfc721da47c8e
Francisco <franciscosimoes2002@hotmail.com>
2023-04-26 15:09:13 +0100

    Add test Unit

---
+++ src/test/java/pt/franciscosimoes/chatgpt/mapper/OpenAiRequestToJasonStringTest.java
@@ -0,0 +1,14 @@
+package pt.franciscosimoes.chatgpt.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OpenAiRequestToJasonStringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
